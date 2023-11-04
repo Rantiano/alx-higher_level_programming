e0100177de267e52fe4ccb46e3a1f87a0ae8e570
Rantiano <rantianoo@gmail.com>
2023-11-04 08:13:07 -0700

    More Classes and Objects

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Defines the class of an empty Rectangle.
+"""
+
+
+class Rectangle:
+    """Empty Rectangle class."""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,4 @@
+This is 0x08. Python - More Classes and Objects on Python
+ By: Guillaume
+ Weight: 1
+ Done by me: Ogunsanya Ranti
